3e26deb810951dfd0e093c0272941daaf8c11607
María Alejandra Acosta Sáez <101402914+Ale1988Acosta@users.noreply.github.com>
2022-05-03 23:58:32 -0400

    Add files via upload

--- Java/java_j.js
+++ Java/java_j.js
@@ -1,107 +1,122 @@
-var nom = document.getElementById("nom");
-
-var ap = document.getElementById("ap");
-
-var cont = document.getElementById("cont");
-
-var rcont = document.getElementById("rcont");
- 
-const form1 = document.getElementById("form1");
-const form2 = document.getElementById("form2");
-
-var msj = document.getElementById("mensaje");
-var msj1 = document.getElementById("mensaje1");
-
-var correo = document.getElementById("email");
-var valcorreo = /[\w-\.]{2,}@[a-zA-Z0-9._-]+\.[\w-]{2,4}/;
-var esValido = valcorreo.test(correo)
 
 
-var rut = document.getElementById("rut")
 
-
-form1.addEventListener("submit" , e/* nombre, puede ser cualquiera*/  => {
+$(document).ready(function(){
+    form1.addEventListener("submit" , e/* nombre, puede ser cualquiera*/  => {
     e.preventDefault(); /* prevenir el envio del formulario */
     let mensajeMostrar ="";
     let entrar = false;
+    var nom = $("#nom").val();
+    var ap = $("#ap").val();
+    var cont = $("#cont").val();
+    var rcont = $("#rcont").val();
+    var comuna = $("#comuna").val();
+    var direccion = $("#direccion").val();
+    var correo = $("#email").val();
+    var valnombre = /^[a-zA-ZÀ-ÿ\s]{1,30}$/;
 
     /* Validar correo */
+    var correo= $("#email").val();
+    var num = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    if(!num.test(correo)){  
+        mensajeMostrar +="Debe ingresar un correo invalido<br>";
+        entrar=true;
+    }
 
-    if(esValido == true){
-        mensajeMostrar += "<br>El correo electronico es invalido<br>";
+    /* Longitud de la contraseña */
+    if(cont.trim().length<8 || cont.trim().length >16){
+        mensajeMostrar += "La longitud de la contraseña debe ser entre 8 y 16 caracteres<br>";
         entrar = true;
     }
-    /* Longitud del nombre */
-    if(cont.value.length < 8 || cont.value.length > 16){
-        mensajeMostrar += "<br>La longitud de la contraseña debe ser entre 8 y 16 caracteres<br>";
+
+    if(rcont.trim().length<8 || rcont.trim().length >16){
+        mensajeMostrar += "La longitud de la contraseña debe ser entre 8 y 16 caracteres<br>";
         entrar = true;
     }
-        /* igualdad contraseñas */
-        if(cont.value != rcont.value){
-            mensajeMostrar += "<br>Las contraseñas deben ser iguales<br>";
-            entrar = true;
-        }
-    /* Longitud del nombre */
-    if(nom.value.length < 4 || nom.value.length > 7){
-        mensajeMostrar += "<br>La longitud del nombre no es correcta <br>";
+
+    /* igualdad contraseñas */
+    if(cont.value != rcont.value){
+        mensajeMostrar += "Las contraseñas deben ser iguales<br>";
         entrar = true;
+    }
+
+    /* Longitud del nombre */
+    if(nom.trim().length<4){
+        mensajeMostrar += "La longitud del nombre no es correcta <br>";
+        entrar = true;
+    }
+
+    if(!valnombre.test(nom)){
+        mensajeMostrar +="En el nombre solo debes ingresar letras<br>";
+        entrar=true;
     }
 
     /* Validar si es mayuscula la primera letra*/
-    var primeraletra = nom.value.charAt(0);
-    if(!esMayuscula(primeraletra)){
-        mensajeMostrar += "<br>La primera letra del nombre es minuscula <br>";
+    var letra = nom.charAt(0);
+    if(!esMayuscula(letra)){
+        mensajeMostrar +="la primera letra del nombre esta en minúscula<br>";
+        entrar=true;
+    }
+
+    /* Longitud del apellido */
+    if(ap.trim().length < 4 ){
+        mensajeMostrar += "La longitud del apellido no es correcta <br>";
         entrar = true;
     }
 
-        /* Longitud del apellido */
-        if(ap.value.length < 4 ){
-            mensajeMostrar += "<br>La longitud del apellido no es correcta <br>";
-            entrar = true;
-        }
+    if(!valnombre.test(ap)){
+        mensajeMostrar +="En el apellido solo debes ingresar letras<br>";
+        entrar=true;
+    }
     
-        /* Validar si es mayuscula del apellido la primera letra*/
-        var primeraletra = ap.value.charAt(0);
-        if(!esMayuscula(primeraletra)){
-            mensajeMostrar += "<br>La primera letra del apellido es minuscula <br>";
-            entrar = true;
-        }
+    /* Validar si es mayuscula del apellido la primera letra*/
+    var letra = ap.charAt(0);
+        if(!esMayuscula(letra)){
+        mensajeMostrar +="la primera letra del apellido esta en minúscula<br>";
+        entrar=true;
+    }
 
-        
+    //direccion
+    var numer= /^[a-zA-Z\d?\s?]+\s?\W?\d+$/;
+    if(!numer.test(direccion)){  
+        mensajeMostrar +="Debe ingresar una direccion valida<br>";
+        entrar=true;
+    }
+                
+    if(comuna.trim().length<3){
+        mensajeMostrar += "No existen comunas con solo 3 letras<br>";
+        entrar=true;
+    }
 
-    /* Mostrar el mensaje en el HTML*/
+    if(!valnombre.test(comuna)){
+        mensajeMostrar +="En la comuna solo debes ingresar letras<br>";
+        entrar=true;
+    }
+
     if(entrar){
-        msj.innerHTML = mensajeMostrar;
+        $("#mensaje").html(mensajeMostrar);
+    }else{
+        $("#mensaje").html("Datos actualizados correctamente");
     }
-    else{
-        msj.innerHTML = "<br>Datos guardados correctamente";
-    }
-
 });
 
+    function esMayuscula(letra){
+        return letra == letra.toUpperCase();
+    }
 
 form2.addEventListener("submit" , a/* nombre, puede ser cualquiera*/  => {
     a.preventDefault(); /* prevenir el envio del formulario */
     let mensajeMostrar1 ="";
     let entrar1 = false;
 
-    /* Longitud del rut */
-    if(rut.value.length < 11 || rut.value.length > 13){
-        mensajeMostrar1 += "<br>La longitud del rut no es correcta <br>";
-        entrar1 = true;
-    }
-        
-
     /* Mostrar el mensaje en el HTML*/
     if(entrar1){
         msj1.innerHTML = mensajeMostrar1;
     }
     else{
-        msj1.innerHTML = "<br>Datos guardados correctamente";
+        msj1.innerHTML = "Datos guardados correctamente";
     }
 });
 
-/* Funcion para preguntar si la letra es mayuscula*/ 
-function esMayuscula(letra){
-    return letra == letra.toUpperCase();
-}
+
+})
