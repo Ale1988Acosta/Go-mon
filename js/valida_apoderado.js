cb99f39dd32a657840658d2f8e33df46422a322a
María Alejandra Acosta Sáez <101402914+Ale1988Acosta@users.noreply.github.com>
2022-05-04 08:17:00 -0400

    Add files via upload

--- js/valida_apoderado.js
+++ js/valida_apoderado.js
@@ -20,11 +20,18 @@ $(document).ready(function(){
         let mensajes ="";
         let entrar = false;
         var nom = $("#nombre").val();
+        var ap = $("#p_apellido").val();
 
         //nombre
-        if(nom.trim().length<3){
+        if(nom.trim().length<4){
             mensajes += "El nombre debe contener mas de tres letras<br>";
             entrar=true;
+        }
+
+        var letra = nom.charAt(0);
+        if(!esMayuscula(letra)){
+        mensajes +="la primera letra del nombre esta en minúscula<br>";
+        entrar=true;
         }
 
         //p_apellido
@@ -32,7 +39,13 @@ $(document).ready(function(){
             mensajes += "Debe ingresar el primer apellido<br>";
             entrar=true;
         }
-        
+
+        var letra = ap.charAt(0);
+        if(!esMayuscula(letra)){
+        mensajes +="la primera letra del apellido esta en minúscula<br>";
+        entrar=true;
+        }
+
         //s_apellido
         if($("#s_apellido").val().trim().length==0){
             mensajes += "Debe ingresar el segundo apellido<br>";
@@ -67,8 +80,6 @@ $(document).ready(function(){
 
         //contraseña
         var pass= $("#password").val();
-        var nume = /^.{8,16}$/;
-        
         if(pass.trim().length<8 || pass.trim().length >16){
             mensajes += "El largo de la contraseña debe ser mas que 8 y menos que 16 <br>";
             entrar=true;
@@ -86,17 +97,21 @@ $(document).ready(function(){
 
         var com= $("#comuna").val();
         if(com.trim().length<3){
-            mensajes += "No existen comunas con solo 2 letras<br>";
+            mensajes += "No existen comunas con solo 3 letras<br>";
             entrar=true;
         }
 
         if(entrar){
             $("#mensaje").html(mensajes);
         }else{
-            $("#mensaje").html("Formulario enviado");
+            $("#mensaje").html("Datos actualizados correctamente");
         }
 
     });
+
+    function esMayuscula(letra){
+        return letra == letra.toUpperCase();
+    }
 })
 
 
